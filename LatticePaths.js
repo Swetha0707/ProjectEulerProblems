90cfb1432e88c1ade892fdbb15d5b1b08427d4b6
Swetha0707 <103947484+Swetha0707@users.noreply.github.com>
2022-04-24 18:03:45 +0530

    Add files via upload

---
+++ LatticePaths.js
@@ -0,0 +1,17 @@
+
+/* problem Statement
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+
+How many such routes are there through a 20×20 grid?
+
+*/
+const gridSize = 20;
+let paths = 1;
+ 
+for (let i = 0; i < gridSize; i++) {
+    paths *= (2 * gridSize) - i;
+    paths /= i + 1;
+}
+console.log("In a 20*20 grid there are " +paths +" lattice paths");
